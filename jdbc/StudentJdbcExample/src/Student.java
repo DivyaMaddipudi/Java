79d94629622722f7b45a6de2c37e13a04bb98626
SatyaDivya <divya.maddipudi1@gmail.com>
2020-05-24 14:43:34 +0530

    jdbc files added

---
+++ jdbc/StudentJdbcExample/src/Student.java
@@ -0,0 +1,33 @@
+
+public class Student {
+	private String firstname;
+	private String lastname;
+	int age;
+		
+	public Student() {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.age = age;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
--- jdbc/StudentJdbcExample/src/StudentDetails.java
+++ jdbc/StudentJdbcExample/src/StudentDetails.java
@@ -1,4 +1,52 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class StudentDetails {
+	public static void main(String[] args) throws Exception {
+		
+		List<Student> studentList = new ArrayList<Student>();
+		
+		Statement st = null;
+		ResultSet rs = null;
+		String query = "SELECT * FROM STUDENT";
+		
+		
+		String url = "jdbc:mysql://localhost:3306/example";
+		String uname = "student";
+		String pass = "student";
+		
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url,uname,pass);
+			st = con.createStatement();
+			rs = st.executeQuery(query);
+			
+			while(rs.next()) {
+				
+				Student s = new Student();
+				s.setFirstname(rs.getString("firstname"));
+				s.setLastname(rs.getString("lastname"));
+				s.setAge(rs.getInt("age"));
+				
+				studentList.add(s);
+			}
+				
+				for(Student value: studentList) {
+					System.out.print("FirstName: "+value.getFirstname()+ ", ");
+					System.out.print("LastName: "+value.getLastname()+ ", ");
+					System.out.print("age: "+value.getAge());
+					System.out.println();
+				
+			}
+			
+		} catch (Exception e) {
+			System.out.println("error");
+			System.out.println(e.getMessage());
+		}
+	}
 
 }
