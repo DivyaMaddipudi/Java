4ebcf513ef94fa83f26706e46075fd44d24868d7
SatyaDivya <divya.maddipudi1@gmail.com>
2020-06-06 15:54:59 +0530

    Sample file modified

--- Java HackerRank/Sample/src/Sample.java
+++ Java HackerRank/Sample/src/Sample.java
@@ -2,42 +2,34 @@ import java.util.*;
 
 class Sample { 
 	
-	public static void main (String[] args) { 
-	{ 
+	public static void main (String[] args) { 		
 		
-		int arr[] = {2, 4, 5, 10, 10, 90, 2, 10, 2}; 
+
+		int arr[] = {2, 4, 5, 10, 10, 90, 2, 10, 2, 2}; 
 		int n = arr.length; 
-		
 		
 		Arrays.sort(arr); 
 		 
-		int max_count = 1, res = arr[0]; 
-		int curr_count = 1; 
+
+		int largest = 0;
+		int k = 0;
 		
-		for (int i = 1; i < n; i++) 
+		for (int i = 0; i < n; i++) 
 		{ 
-			if (arr[i] == arr[i - 1]) 
-				curr_count++; 
-			else
-			{ 
-				if (curr_count > max_count) 
-				{ 
-					max_count = curr_count; 
-					res = arr[i - 1]; 
-				} 
-				curr_count = 1; 
-			} 
-		} 
-	
-		 
-		if (curr_count > max_count) 
-		{ 
-			max_count = curr_count; 
-			res = arr[n - 1]; 
-		} 
-	
-		System.out.println(res + " appeared " + max_count + " times ");
-	} 
-	
+			int count = 1;
+			
+			for (int j = i + 1; j < n; j++) {
+		        if (arr[i] == arr[j]) {
+		            count++;
+		        }
+		    }
+			if (count > largest) {
+		        k = arr[i];
+		        largest = count;
+		    }
+		}
+		
+		
+		System.out.println(k + " " + largest);
 	} 
 } 
