ffc8e73a4415495cef6a1b554f36e2a573ebf064
SatyaDivya <divya.maddipudi1@gmail.com>
2020-06-07 22:29:01 +0530

    Good vowel string program added

--- Java HackerRank/HackerEarth-Little Monk and Good String/src/GoodVowelString.java
+++ Java HackerRank/HackerEarth-Little Monk and Good String/src/GoodVowelString.java
@@ -1,36 +1,41 @@
-import java.util.Scanner;
-
-class GoodVowelString {
-	
-	static char[] vow = new char[]{'a','e','i','o','u'};
-    public static void main(String args[] ) throws Exception {
-
-
-        Scanner s = new Scanner(System.in);
-        String str = s.nextLine();    
-        
-        String res = "";
-        for(int i=0;i<str.length();i++) {
-            for(int j=i+1;j<=str.length();j++) {
-                res = res + str.substring(i,j) + " ";
-            }
-        }
-        String[] data = res.split(" ");
-        
-        for(String x: data) {
-        	System.out.println(vowel(x));
-        }
+import java.util.*;
+	class GoodVowelString
+	{
+	public static void main(String args[] ) throws Exception
+	{
+	Scanner s=new Scanner(System.in);
+	String str=s.next();
+	int i=0,temp_count=0,count=0;
+	while(i<str.length())
+	{
+	if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u')
+	{
+	temp_count++;
+	i++;
+	while(temp_count!=0&&i<str.length())
+	{
+	if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u')
+	{
+	temp_count++;
+	i++;
 	}
-	public static String vowel(String x) {
-		String val = "";
-		
+	else
+	{
+	if(count<temp_count)
+	count=temp_count;
+	temp_count=0;
+	// System.out.println(count);
+	}
+	}
 
-		for(int i=0;i<x.length();i++) {
-			if(x == vow[i]) {
-				
-			}
-			
-		}
-		return val;
-    }
-}
+	}
+	i++;
+	}
+	if(temp_count>count)
+	count=temp_count;
+	System.out.println(count);
+
+	}
+
+	}
+
