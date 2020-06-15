55a0a02b9332ac15b87c52bbd3cbfd5e02fd2e1b
SatyaDivya <divya.maddipudi1@gmail.com>
2020-06-15 11:16:17 +0530

    added programs

--- Java HackerRank/GCD/src/GCD.java
+++ Java HackerRank/GCD/src/GCD.java
@@ -1,18 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class GCD {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int i1 = sc.nextInt();
-		int i2 = sc.nextInt();
 		
-		while(i1 != i2) {
-			if(i1>i2) {
-				i1 = i1 - i2;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		while(num1 != num2) {
+			if(num1 > num2) {
+				num1 = num1 - num2;
 			} else {
-				i2 = i2 - i1;
+				num2 = num2 - num1;
 			}
+			
 		}
-		System.out.println(i2);
+		System.out.println(num2);
+		
+		
 	}
 }
