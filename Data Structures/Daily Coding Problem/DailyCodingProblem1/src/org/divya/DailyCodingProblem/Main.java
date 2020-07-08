b5b692a4ab231694d330a506b184e39fbe0385d8
SatyaDivya <divya.maddipudi1@gmail.com>
2020-07-08 21:08:52 +0530

    Daily Coding Problem2 Added

--- Data Structures/Daily Coding Problem/DailyCodingProblem1/src/org/divya/DailyCodingProblem/Main.java
+++ Data Structures/Daily Coding Problem/DailyCodingProblem1/src/org/divya/DailyCodingProblem/Main.java
@@ -5,11 +5,11 @@
 	For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
  */
 
-package org.divya.solutions;
+package org.divya.DailyCodingProblem;
 
 import java.util.*;
 
-public class Problem1 {
+public class Main {
 	
 	public static void findValues(List<Integer> list, int k) {
 		int count = 0;
