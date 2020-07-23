9b0bccc6abdb2b28588f707cac33033ff8ed4b14
SatyaDivya <divya.maddipudi1@gmail.com>
2020-07-23 15:13:30 +0530

    images added to file system

--- Maven/32ImageUpload/src/divya/controller/ImageUpload.java
+++ Maven/32ImageUpload/src/divya/controller/ImageUpload.java
@@ -1,5 +1,6 @@
 package divya.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -9,32 +10,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.tomcat.util.http.fileupload.FileItem;
-import org.apache.tomcat.util.http.fileupload.FileUploadException;
-import org.apache.tomcat.util.http.fileupload.RequestContext;
-import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
-import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
-
+/**
+ * Servlet implementation class ImageUpload
+ */
 @WebServlet("/ImageUpload")
 public class ImageUpload extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	ServletFileUpload upload = new ServletFileUpload(new DiskFileItemFactory());
-	
-	
-	try {
-		List<FileItem> images = upload.parseRequest((RequestContext) request);
-		for(FileItem image: images) {
-			System.out.println(image.getName());
-		}
-	
-	} catch (FileUploadException e) {
+		ServletFileUpload upload = new ServletFileUpload(new DiskFileItemFactory());
 		
-		e.printStackTrace();
-	}
+		
+		try {
+			List<FileItem> images = upload.parseRequest(request);
+			for(FileItem image: images) {
+				
+				String name = image.getName();
+				try {name = name.substring(name.lastIndexOf("\\")+1);} catch(Exception e) {}
+				image.write(new File("c:/images/" + name));
+			}
+		
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
 	}
 
 }
