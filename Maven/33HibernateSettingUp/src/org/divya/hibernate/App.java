3dedfb3728d839f6de249bc5e9b1d9be15917fc5
SatyaDivya <divya.maddipudi1@gmail.com>
2020-07-24 12:19:44 +0530

    create and read operations added

---
+++ Maven/33HibernateSettingUp/src/org/divya/hibernate/App.java
@@ -0,0 +1,50 @@
+package org.divya.hibernate;
+
+import org.divya.hibernate.entity.Hibusers;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+													.addAnnotatedClass(Hibusers.class)
+													.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			/*
+			//Adding data into db.
+			// Create object of entity class type
+			Hibusers user = new Hibusers("username", "password", "firstName", "lastName");
+			
+			// Start transaction
+			session.beginTransaction();
+			// Perform operation
+			session.save(user);
+			// Commit the transaction
+			session.getTransaction().commit();
+			System.out.println("Row added!");
+			*/
+			
+			//Retriving data from db.
+			
+			Hibusers user = new Hibusers();
+			
+			session.beginTransaction();
+			
+			user = session.get(Hibusers.class, 4);
+			
+			session.getTransaction().commit();
+			System.out.println(user);
+			
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
--- Maven/33HibernateSettingUp/src/org/divya/hibernate/entity/Hibusers.java
+++ Maven/33HibernateSettingUp/src/org/divya/hibernate/entity/Hibusers.java
@@ -6,23 +6,84 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="hibusers")
+@Table(name = "hibusers")
 public class Hibusers {
-	
+
 	@Id
-	@Column(name="users_id")
+	@Column(name = "users_id")
 	int usersId;
-	
-	@Column(name="username")
+
+	@Column(name = "username")
 	String username;
-	
-	@Column(name="password")
+
+	@Column(name = "password")
 	String password;
-	
-	@Column(name="first_name")
+
+	@Column(name = "first_name")
 	String firstName;
-	
-	@Column(name="last_name")
+
+	@Column(name = "last_name")
 	String lastName;
+	
+	
+	public Hibusers() {
+
+	}
+
+	public Hibusers(String username, String password, String firstName, String lastName) {
+		
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getUsersId() {
+		return usersId;
+	}
+
+	public void setUsersId(int usersId) {
+		this.usersId = usersId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Hibusers [usersId=%s, username=%s, password=%s, firstName=%s, lastName=%s]", usersId,
+				username, password, firstName, lastName);
+	}
+	
+	
 
 }
